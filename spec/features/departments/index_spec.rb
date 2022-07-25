b17b32c161d6d1dfcb39ebe28539637523b0f819
Gauri Joshi <gauri728@gmail.com>
2022-07-25 08:53:26 -0700

    user story 1

--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -25,5 +25,29 @@ RSpec.describe 'Department index page' do
       expect(page).to have_content("3")
       expect(page).to_not have_content("4")
    end
+
+   it 'has names of departments employees underneath department' do 
+      department_1 = Department.create!(name: "Sales", floor: 2)
+      department_2 = Department.create!(name: "Inventory", floor: 1)
+      department_3 = Department.create!(name: "Returns", floor: 3)
+
+      employee_1 = department_1.employees.create!(name: "Susan", level: 5)
+      employee_2 = department_2.employees.create!(name: "Rohan", level: 2)
+      employee_3 = department_3.employees.create!(name: "Lola", level: 4)
+
+      visit '/departments'
+
+      within "#department-#{department_1.id}" do 
+         expect(page).to have_content("Susan")
+      end
+
+      within "#department-#{department_2.id}" do 
+         expect(page).to have_content("Rohan")
+      end
+
+      within "#department-#{department_3.id}" do 
+         expect(page).to have_content("Lola")
+      end
+   end
 end
       
